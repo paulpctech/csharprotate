9a5eaa29735eb9c6277ca4e15e263dc8849dd4c1
Paul Clarence <Paul Clarence@DESKTOP-RBAD5RE>
2021-12-31 02:55:19 +0800

    new push

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] nums = { 5, 4, 3 };
+            Array.Reverse(nums, 0, 3);
+            Console.Write("Array: [{0}]", string.Join(",", nums));
+        }
+    }
+}
